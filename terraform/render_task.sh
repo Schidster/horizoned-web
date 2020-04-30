4e6c5285b56f60fe271b5ec183c72c1f3fda4e7f
Schidster Adinson <schid2003@gmail.com>
2020-04-30 12:21:28 +0530

    Fix errors

--- terraform/render_task.sh
+++ terraform/render_task.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Replace image from environment variable in task-definition
-sed -i "s/web:latest/$WEB_IMAGE/g" terraform/task-definition.json
+sed -i "s/web:latest/$WEB_IMAGE/g/" terraform/task-definition.json
